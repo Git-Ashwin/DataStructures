4a19b6d58281e5348d29c286d99a4d6bfd53fddd
Ashwin <2017ashwin@gmail.com>
2019-04-08 18:25:39 +0530

    take 1st element as source node

--- graph.py
+++ graph.py
@@ -8,15 +8,15 @@ class graph:
         visited={}
         for i in self.graph.keys():
             visited[i]=False
-        for i in self.graph.keys():
-            if visited[i]==False:
+        i=list(self.graph.keys())[0]
+        if visited[i]==False:
                  print(i)
                  visited[i]=True
                  stack=[]
                  for j in self.graph[i]:
                     if visited[j]==False and j not in stack:
                         stack.append(j)
-            while stack:
+                 while stack:
                     s=stack.pop()
                     for j in self.graph[s]:
                        if visited[j]==False and j not in stack:
@@ -27,8 +27,8 @@ class graph:
         visited={}
         for i in self.graph.keys():
             visited[i]=False
-        for i in self.graph.keys():
-            if visited[i]==False:
+        i=list(self.graph.keys())[0]
+        if visited[i]==False:
                 print(i)
                 visited[i]=True
                 stack=[]
@@ -50,10 +50,12 @@ a=graph()
 a.addedge(1,2)
 a.addedge(2,1)
 a.addedge(1,5)
+a.addedge(5,1)
 a.addedge(1,10)
 a.addedge(10,1)
 a.addedge(4,5)
 a.addedge(5,4)
 a.addedge(3,1)
-a.dfs()
+a.addedge(1,3)
+a.bfs()
 print(a.graph)
