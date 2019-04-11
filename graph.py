7ae606fc23692ffc3ac0661454a61d1f1add1f4c
Ashwin <2017ashwin@gmail.com>
2019-04-11 22:18:20 +0530

    update graph

--- graph.py
+++ graph.py
@@ -4,58 +4,55 @@ class graph:
         self.graph=defaultdict(list)
     def addedge(self,u,v):
         self.graph[u].append(v)
+        self.graph[v].append(u)
     def dfs(self):
         visited={}
         for i in self.graph.keys():
             visited[i]=False
         i=list(self.graph.keys())[0]
-        if visited[i]==False:
-                 print(i)
-                 visited[i]=True
-                 stack=[]
-                 for j in self.graph[i]:
-                    if visited[j]==False and j not in stack:
-                        stack.append(j)
-                 while stack:
-                    s=stack.pop()
-                    for j in self.graph[s]:
-                       if visited[j]==False and j not in stack:
-                        stack.append(j)
-                    print(s)
-                    visited[s]=True
+        print(i)
+        visited[i]=True
+        stack=[]
+        for j in self.graph[i]:
+            if visited[j]==False and j not in stack:
+                stack.append(j)
+        while stack:
+            s=stack.pop()
+            for j in self.graph[s]:
+                if visited[j]==False and j not in stack:
+                     stack.append(j)
+            print(s)
+            visited[s]=True
+
     def bfs(self):
         visited={}
         for i in self.graph.keys():
             visited[i]=False
         i=list(self.graph.keys())[0]
-        if visited[i]==False:
-                print(i)
-                visited[i]=True
-                stack=[]
-                for j in self.graph[i]:
-                    if visited[j]==False and j not in stack:
+        print(i)
+        visited[i]=True
+        stack=[]
+        for j in self.graph[i]:
+           if visited[j]==False and j not in stack:
+                 stack.append(j)
+           while stack:
+              s=stack.pop(0)
+              for j in self.graph[s]:
+                 if visited[j]==False and j not in stack:
                         stack.append(j)
-                while stack:
-                    s=stack.pop(0)
-                    for j in self.graph[s]:
-                        if visited[j]==False and j not in stack:
-                                stack.append(j)
-                    print(s)
-                    visited[s]=True
+              print(s)
+              visited[s]=True
+                 
                         
 
     
         
 a=graph()
 a.addedge(1,2)
-a.addedge(2,1)
-a.addedge(1,5)
-a.addedge(5,1)
-a.addedge(1,10)
-a.addedge(10,1)
-a.addedge(4,5)
-a.addedge(5,4)
-a.addedge(3,1)
 a.addedge(1,3)
-a.bfs()
-print(a.graph)
+a.addedge(1,4)
+a.addedge(2,5)
+a.addedge(4,6)
+a.addedge(4,5)
+
+a.dfs()
