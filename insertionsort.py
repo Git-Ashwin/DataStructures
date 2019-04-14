cbbf228c10c1cb333eb756dab7da25cb6db9c96a
Ashwin <Git-Ashwin@github.com>
2019-04-14 07:04:38 +0530

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,10 +1,11 @@
 a=[5,6,7,1,1,10,2,3,0]
-
-for i in range(1,len(a)):
+def insertionsort(a):
+ for i in range(1,len(a)):
      M=a[i]
      while M<a[i-1] and i>0:
         a[i]=a[i-1]
         i=i-1
 
      a[i]=M
+insertionsort(a)
 print(a)
