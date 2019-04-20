9157c42932d27bc8fbaf7378231910e87a315f11
git-ashwin <2017asshwin@gmail.com>
2019-04-20 16:44:12 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,6 +1,6 @@
 a=[10,10,20,4,2,7,6,3]
 def bubblesort(a):
- for i in range(len(a)-1):
+ for _ in range(len(a)-1):
     for j in range(len(a)-1):
         if a[j]>a[j+1]:
             a[j],a[j+1]=a[j+1],a[j]
